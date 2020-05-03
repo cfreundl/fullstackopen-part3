c9d832e716ab127e8ce56929338cbf95eae325af
Christoph Freundl <Christoph.Freundl@gmx.de>
2020-05-03 23:08:20 +0200

    Exercise 3.4
    phonebook backend step 4

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
     res.send(`
 Phonebook has info for ${persons.length} people<br>
