4fdbab83ae51368a0b00a31173920568624eda59
Christoph Freundl <Christoph.Freundl@gmx.de>
2020-05-08 19:21:53 +0200

    Exercise 3.11
    phonebook backend step 11
    Added frontend production build

---
+++ build/precache-manifest.f1731f2ddcce621069010c3124931723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c066c7234f68bcf4964906af05c6e5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38cefce44aad7904b09",
+    "url": "/static/css/main.73f19f2b.chunk.css"
+  },
+  {
+    "revision": "f5ffb0dc48ad9952bcfd",
+    "url": "/static/js/2.5c68fe71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c68fe71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38cefce44aad7904b09",
+    "url": "/static/js/main.2cd7546a.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
