fd3b8207ae4d61b16440c31336d0f9cf3dd4b0a5
Christoph Freundl <Christoph.Freundl@gmx.de>
2020-05-11 21:50:00 +0200

    Exercise 3.15
    phonebook database step 3

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 const Person = require('./models/person')
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
-app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -57,10 +55,14 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).end()
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(400).send({ error: 'malformatted id'})
+    })
 })
 
 app.post('/api/persons', (req, res) => {
