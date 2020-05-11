4909ddacc7bb548fb03496043e622459f5f0f6af
Christoph Freundl <Christoph.Freundl@gmx.de>
2020-05-11 22:24:03 +0200

    Exercise 3.17
    phonebook database step 5

--- index.js
+++ index.js
@@ -51,6 +51,25 @@ app.get('/api/persons/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+        if (updatedPerson) {
+            res.json(updatedPerson.toJSON())
+        } else {
+            res.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
     .then(result => {
